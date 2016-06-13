2407c5929a7f35b298d24e0980c90cd79b7a93c4
Jerin Philip <monu1618@gmail.com>
2016-06-13 14:09:40 +0530

    GIS elements added

--- scripts/schema.sql
+++ scripts/schema.sql
@@ -1,29 +1,28 @@
+-- Interest Point Information
 CREATE TABLE ip_desc(
     Id INTEGER PRIMARY KEY AUTOINCREMENT,
     Title STRING,
     Description TEXT
 );
 
--- Locations Table
+-- Interest Point Locations
 CREATE TABLE ip_locs(
     Id INTEGER,
     Location POINT,
     FOREIGN KEY(Id) REFERENCES ip_desc(Id)
 );
 
-
+-- Images Information
 CREATE TABLE images(
     Id INTEGER PRIMARY KEY AUTOINCREMENT,
     Caption TEXT,
     FsLocation STRING
 );
 
+-- Image Locations
 CREATE TABLE img_locs(
     Id INTEGER,
     Location POINT,
     FOREIGN KEY(Id) REFERENCES images(Id)
 );
 
-
-INSERT INTO ip_desc (Title, Description) VALUES("Vindhya Canteen", "Shitty Food");
-INSERT INTO ip_locs (Id, Location) VALUES(1, GeomFromText('POINT(1 1)'));
